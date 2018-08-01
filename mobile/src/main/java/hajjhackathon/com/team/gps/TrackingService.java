ef58b648ab3ee3e721926f3f26c08c0710202c05
Karimmagdi <karim.magdi87@gmail.com>
2018-08-01 19:24:40 +0200

    - Fix Location Version

--- mobile/src/main/java/hajjhackathon/com/team/gps/TrackingService.java
+++ mobile/src/main/java/hajjhackathon/com/team/gps/TrackingService.java
@@ -26,6 +26,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import hajjhackathon.com.team.R;
+
 
 public class TrackingService extends Service {
 
